d2320df002e8d17c4cdc1182f44d90578758f496
varadha2k <varadharajan@gmail.com>
2018-04-07 01:19:56 +0530

    intial version

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
+
+import {
+    DemoSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        DemoSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'en'
+        },
+    ],
+    exports: [
+        DemoSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class DemoSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
